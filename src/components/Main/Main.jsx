c19c308468d32a9f265bced5d1035847163a1298
Sebas Taboada <sebastiantaboada.@gmail.com>
2023-09-04 17:20:07 -0300

    MOD main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,30 @@
-
+import locomotiveScroll from 'locomotive-scroll';
+import { useEffect } from 'react';
 
 import './Main.scss'
+
+
+
+
+
 export const Main = (props) => {
 
+    useEffect(()=>{
+
+        const scroll = new locomotiveScroll({
+            el: document.querySelector('#main__container'), // Reemplaza con el selector de tu contenedor de desplazamiento
+            smooth: true, // Habilitar el desplazamiento suave
+        });
+
+        return () => {
+            scroll.destroy()
+        };
+    },[])
+
+
     return (
-        <main className='main'>
-            {/* <div className='main__container'> */}
+        <main className='main__container'  data-scroll-container id='main__container'>
                 {props.children}
-            {/* </div> */}
         </main>
 
     )
